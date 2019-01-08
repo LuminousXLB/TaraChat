2e56fb5d5fb052a07214e48879f6fd66888a8169
Taraxacum <john980118@163.com>
2019-01-09 05:32:17 +0800

    fail to transfer file

--- tarachatServer/servants/common.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  successResponse: payload => {
-    return JSON.stringify({ success: 1, payload })
-  },
-  failResponse: error => {
-    return JSON.stringify({ success: 0, payload: { error } })
-  }
-}
